95256e37336bc0cd9aaabe4aec4d7073977d8442
c <ichlings@qq.com>
2024-04-20 21:14:43 +0800

    demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use microbit as _;
+use panic_halt as _;
+
+#[entry]
+fn main() -> ! {
+    let _y;
+    let x = 42;
+    _y = x;
+    loop {}
 }
