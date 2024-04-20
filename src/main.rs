8c7089ef098fbedbeef58d82cdca20ba3bb6ef61
c <ichlings@qq.com>
2024-04-20 22:00:29 +0800

    demo

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,28 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use microbit as _;
-use panic_halt as _;
+use microbit::board::Board;
+use microbit::hal::prelude::*;
+use microbit::hal::timer::Timer;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
-    loop {}
+    rtt_init_print!();
+    let mut board = Board::take().unwrap();
+
+    let mut timer = Timer::new(board.TIMER0);
+
+    board.display_pins.col1.set_low().unwrap();
+    let mut row1 = board.display_pins.row1;
+
+    loop {
+        row1.set_low().unwrap();
+        rprintln!("Dark!");
+        timer.delay_ms(1_000_u16);
+        row1.set_high().unwrap();
+        rprintln!("Light!");
+        timer.delay_ms(1_000_u16);
+    }
 }
