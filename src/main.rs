21ca17cb37a42a77eb30a6e7fc0614d262880794
c <iclings@qq.com>
2024-11-29 22:05:13 +0800

    embed

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,28 @@
 use cortex_m_rt::entry;
 use microbit::{board::Board, display::blocking::Display, hal::Timer};
 use panic_halt as _;
-use rtt_target::rtt_init_print;
 
 const PIXELS: [(usize, usize); 16] = [
-    (0, 0),(0, 1),(0, 2),(0, 3),
-    (0, 4),(1, 4),(2, 4),(3, 4),
-    (4, 4),(4, 3),(4, 2),(4, 1),
-    (4, 0),(3, 0),(2, 0),(1, 0),
+    (0, 0),
+    (0, 1),
+    (0, 2),
+    (0, 3),
+    (0, 4),
+    (1, 4),
+    (2, 4),
+    (3, 4),
+    (4, 4),
+    (4, 3),
+    (4, 2),
+    (4, 1),
+    (4, 0),
+    (3, 0),
+    (2, 0),
+    (1, 0),
 ];
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
-
     let board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
     let mut display = Display::new(board.display_pins);
